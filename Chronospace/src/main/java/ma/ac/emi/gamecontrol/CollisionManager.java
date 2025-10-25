eb78c44636b45128c368d0c9293dc101d1fa6a74
amine204-204 <amine.belh204@gmail.com>
2025-10-25 19:44:55 +0100

    add collision player&projectiles and ennemy&projectiles

---
+++ Chronospace/src/main/java/ma/ac/emi/gamecontrol/CollisionManager.java
@@ -0,0 +1,41 @@
+package ma.ac.emi.gamecontrol;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+import ma.ac.emi.gamelogic.entity.Ennemy;
+import ma.ac.emi.gamelogic.player.Player;
+import ma.ac.emi.gamelogic.projectile.Projectile;
+import ma.ac.emi.gamelogic.projectile.ProjectileManager;
+
+@Getter
+@Setter
+public class CollisionManager {
+	private Player player;
+	private List<Ennemy> enemies;
+	private ProjectileManager projectileManager;
+	
+	public void handleCollisions() {
+		for(Projectile projectile : projectileManager.getEnemyProjectiles()) {
+			if(player.getBound().intersects(projectile.getBound())) {
+				//Apply damage
+				
+				//Desactivate projectile
+				projectile.setActive(false);
+			}
+		}
+		
+		for(Projectile projectile : projectileManager.getPlayerProjectiles()) {
+			enemies.forEach((enemy) -> {
+				if(enemy.getBound().intersects(projectile.getBound())) {
+					//Apply damage
+					
+					//Desactivate projectile
+					projectile.setActive(false);
+				}
+			});
+			
+		}
+	}
+}
