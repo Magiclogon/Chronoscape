1f3272dd449eaa06dd6a52997f39c976dfb5d8c5
amine204-204 <amine.belh204@gmail.com>
2026-01-30 11:52:56 +0100

    player config

--- Chronospace/src/main/java/ma/ac/emi/gamelogic/particle/ParticleSystem.java
+++ Chronospace/src/main/java/ma/ac/emi/gamelogic/particle/ParticleSystem.java
@@ -7,7 +7,7 @@ import ma.ac.emi.gamecontrol.GameController;
 import ma.ac.emi.gamecontrol.GameObject;
 import ma.ac.emi.glgraphics.GLGraphics;
 import ma.ac.emi.glgraphics.Texture;
-import ma.ac.emi.glgraphics.lighting.LightParticle;
+import ma.ac.emi.glgraphics.lighting.LightObject;
 import ma.ac.emi.glgraphics.lighting.LightingSystem;
 import ma.ac.emi.math.Vector3D;
 
@@ -61,22 +61,6 @@ public class ParticleSystem {
         double lastTime = lastSpawnTimes.getOrDefault(key, -999.0);
         if (currentTime - lastTime >= 1.0 / def.getSpawnRate()) {
         	Particle p = new Particle(def, new Vector3D(position));
-            activeEffects.add(p);
-            lastSpawnTimes.put(key, currentTime);
-        }
-    }
-    
-    public void spawnLightParticle(Vector3D position, double lightRadius, GameObject source, double currentTime) {
-    	String id = LightParticle.LIGHT_ID;
-    	ParticleDefinition def = definitions.get(id);
-        if (def == null) {
-            System.err.println("Unknown particle effect: " + id);
-            return;
-        }
-        String key = source.hashCode() + ":" + id;
-        double lastTime = lastSpawnTimes.getOrDefault(key, -999.0);
-        if (currentTime - lastTime >= 1.0 / def.getSpawnRate()) {
-        	Particle p = new LightParticle(new Vector3D(position), (float) lightRadius);
             activeEffects.add(p);
             lastSpawnTimes.put(key, currentTime);
         }
